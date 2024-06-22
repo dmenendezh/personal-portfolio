25d46e5857e8ec0f062b624a667304517e7a1060
dmenendezh <diego.andres.menendez@gmail.com>
2024-06-22 13:16:05 -0300

    add portfolio header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from "./logo.svg";
 import "./App.css";
+import "./index.css";
 import React from "react";
+import Header from "./components/Header";
 
+<style>
+  @import
+  url('https://fonts.googleapis.com/css2?family=Onest:wght@100..900&display=swap');
+</style>;
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div
+        className="absolute top-0 bottom-0 z-[-2] min-h-screen w-full bg-gray-50 dark:bg-gray-950
+      bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(217,216,255,0.5),rgba(255,255,255,0.9))]
+      dark:bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"
+      ></div>
+      <Header />
     </div>
   );
 }
